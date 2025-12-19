c34f11dcce33b3d277c731dc948bad4ba974cce2
register-web <noreply@github.com>
2025-12-19 13:53:50 +0300

    фикс обеих частей(13:53)

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "__PASTE_APPS_SCRIPT_EXEC_URL_HERE__";
+const API_URL = "https://script.google.com/macros/s/AKfycbxv9KvD2wZVs9krN0zjksJ_iv2cpfSswQA2CvoqmvzVw2dysTsiLqqV2IP0PW0q_DCxrQ/exec";
